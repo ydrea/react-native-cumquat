829296129972517a125c050ae62e6f9a11777c44
Andrija Vranic <ydrea.wrld@gmail.com>
2026-07-30 15:10:41 +0200

    fix: anchor projection to initial heading

--- cpp/projection/Projection.h
+++ cpp/projection/Projection.h
@@ -6,6 +6,10 @@
 namespace cumquat::projection {
 
 Vec3 worldToCamera(const Vec3& enu, const SensorState& sensorState);
+Quaternion geographicallyAlignedOrientation(
+    const Quaternion& currentOrientation,
+    const Quaternion& initialOrientation,
+    double initialHeadingDegrees);
 bool projectToScreen(
     const Vec3& camera,
     const SensorState& sensorState,
